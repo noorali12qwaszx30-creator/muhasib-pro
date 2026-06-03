4ce98526cbb98a87b10624996b21c6aa864804a9
noorali12qwaszx30-creator <noorali12qwaszx30@gmail.com>
2026-06-03 15:51:44 +0300

    Implement service worker caching strategy

--- Sw.s
+++ Sw.s
@@ -1,3 +1,7 @@
+const CACHE = 'muhasib-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/muhasib-pro/', '/muhasib-pro/index.html'])));
+});
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
